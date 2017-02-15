c2f428a5098fe943d10e5f6b5104b9fa280f5f6d
faraday58 <jarodriguezvera@gmail.com>
2017-02-15 16:30:47 -0600

    Ejemplo Encapsulacióm y Herencia
    Demustra el uso del encapsulamiento y herencia

---
+++ Transportes/Transportes/Automovil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Transportes
+{
+    class Automovil
+    {
+        private float precio;
+        private string color;
+        private string marca;
+        private float velocidad;
+
+        public float Velocidad
+        {
+            get
+            {
+                return velocidad;
+            }
+
+            set
+            {
+                if (value < 0)
+                    velocidad = -value;
+                else
+                velocidad = value;
+            }
+        }
+        public Automovil()
+        {
+            Velocidad = 1;
+        }
+
+
+        public float VelocidadAuto()
+        {
+            Velocidad++; //Velocidad = Velocidad +1;
+            return Velocidad ;
+        }
+
+    }
+}
---
+++ Transportes/Transportes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Transportes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automovil miAuto = new Automovil();
+
+            Console.WriteLine("La velocidad de mi auto es: " + miAuto.VelocidadAuto());
+           
+            Console.WriteLine("La velocidad de mi auto es: " + miAuto.VelocidadAuto());
+            Console.WriteLine("La velocidad de mi auto es: " + miAuto.VelocidadAuto());
+        }
+    }
+}
